fdc55d6787d31d69042ecebcf064dadb609bdcef
bezibaerchen <info@mobil-talk.de>
2019-01-22 12:43:06 +0100

    Update get_default_gateway_wmi.ps1
    Finetuned progress bar
    added and removed counters
    added information output to CLI on finish

--- get_default_gateway_wmi.ps1
+++ get_default_gateway_wmi.ps1
@@ -2,11 +2,19 @@
 
 ## Script to determine name and standard gateway for a given list of IP addresses via WMI and output to CSV
 
+## clear screen
+cls
+
 ## text file containing IP addresses to be checked (one per line)
 $check = Get-Content C:\impex\ips.txt
 
-## set progress counter to 0
+## some counters
 $i=0
+$notconnectable=0
+$gatewaynoname=0
+$namenogateway=0
+$informationavailable=0
+$totalipstocheck=$check.count
 
 ## Output file
 $outfile = "C:\impex\default_gateways.csv"
@@ -24,37 +32,68 @@ echo $csvheaders | Out-File -Append -Encoding default -FilePath $outfile
 
 foreach ($address in $check)
 {
-
+$noname=0
+$nogateway=0
 ## progress bar
 $i++
-Write-Progress -Activity "Working on IP addresses" -Status "Currently working on $address." -PercentComplete ($i/$check.count*100)
+Write-Progress -Activity "Query running..." -Status "Trying for $address ($i of $totalipstocheck) Sucessfully queried: $informationavailable No information available: $notconnectable" -PercentComplete ($i/$check.count*100)
 
 	## try to determine name to IP address via WMI. If not possible, set connectable to 0 and name to n/a
     Try
     {
         $name = Get-WmiObject Win32_ComputerSystem -ComputerName $address -ErrorAction Stop | select -ExpandProperty Name
-        $connectable = 1
     }
     Catch
     {
         $name = "n/a"
-        $connectable = 0
+        $noname=1
     }
 
 	## try to determine default gateway via WMI. If not possible, set connectable to 0 and gateway to n/a
     Try
     {
         $gateway = Get-WmiObject win32_networkAdapterConfiguration -ComputerName $address -ErrorAction Stop |  ?{$_.DefaultIPGateway -ne $null} | select -ExpandProperty DefaultIPGateway
-        $connectable = 1
     }
     Catch
     {
         $gateway = "n/a"
-        $connectable = 0
+        $nogateway=1
     }
+	
+    ## get status and raise counters
+    if ($noname -eq "0" -and $nogateway -eq "0")
+    {
+        $informationavailable++
+        $connectable=1
+    }
+    elseif ($noname -eq "1" -and $nogateway -eq "0")
+    {
+        $informationavailable++
+        $connectable=1
+        $gatewaynoname++
+    }
+    elseif ($noname -eq "0" -and $nogateway -eq "1")
+    {
+        $informationavailable++
+        $connectable=1
+        $namenogateway++
+    }
+    else
+    {
+        $connectable=0
+        $notconnectable++
+    }
+        
+    	
+	
 	## Build CSV line
     $csvline=$address + ";" + $name + ";" + $gateway + ";" + $connectable
+	
+	
 	
 	## append CSV line to output file
     echo $csvline | Out-File -Append -FilePath $outfile
 }
+
+## Output statistics to CLI
+echo "Statistics`nIPs not connectable: $notconnectable`nIPs with gateway information without name: $gatewaynoname`nIPs with name information without gateway: $namenogateway`nIPs with full information available: $informationavailable"
